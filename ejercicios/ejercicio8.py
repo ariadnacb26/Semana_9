9c113de35a9044135f8b20ded20d8cf57ed88e00
ariadnacb26 <ariadnacb@uamv.edu.ni>
2025-05-23 07:31:54 -0600

    Ejercicio 8

---
+++ ejercicios/ejercicio8.py
@@ -0,0 +1,12 @@
+# Lista con valores repetidos
+lista_con_duplicados = [2, 5, 3, 2, 8, 5, 9, 1, 3, 7]
+
+# Crear una nueva lista sin duplicados usando set para eliminar repeticiones
+lista_sin_duplicados = list(set(lista_con_duplicados))
+
+# Mostrar las listas
+print("Lista original con duplicados:")
+print(lista_con_duplicados)
+
+print("\nLista sin duplicados:")
+print(lista_sin_duplicados)

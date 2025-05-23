db65ca0e8af447d27d7430dc8f42d20c31bcd04d
ariadnacb26 <ariadnacb@uamv.edu.ni>
2025-05-23 07:31:07 -0600

    Ejercicio 7

---
+++ ejercicios/ejercicio7.py
@@ -0,0 +1,14 @@
+# Lista de números reales
+numeros = [12.5, 43.8, 9.1, 100.0, 56.7, 78.3, 21.4, 3.9]
+
+# Mostrar la lista
+print("Lista de números reales:")
+print(numeros)
+
+# Encontrar el valor máximo y mínimo
+maximo = max(numeros)
+minimo = min(numeros)
+
+# Mostrar los resultados
+print(f"\nEl número mayor es: {maximo}")
+print(f"El número menor es: {minimo}")
